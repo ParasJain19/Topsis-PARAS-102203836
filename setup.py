d87fee2e116273f6e58c7a38c8e6f6b73ab0d115
ParasJain19 <parasjain190205@gmail.com>
2025-08-07 01:07:48 +0530

    version updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='Topsis-Paras-102203836',
     packages=find_packages(),
-    version='1.0.5',
+    version='1.0.6',
     description='A Python package for TOPSIS',
     long_description=long_description,  
     long_description_content_type='text/markdown',  
